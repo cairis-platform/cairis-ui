82904af67975cae822d2df03ccb1a233e777dc12
Shamal Faily <sfaily@bournemouth.ac.uk>
2019-03-26 13:07:17 +0000

    Fix typo

--- src/store/index.js
+++ src/store/index.js
@@ -24,7 +24,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state : {
     session : '',
-    url : window.location.protocol + "//" + window.location.host,
+    url : window.location.protocol + "//" + window.location.host
 // Uncomment the below when developing/unit testing.
 //    session: 'test',
 //    url: 'http://192.168.161.136:7071'
